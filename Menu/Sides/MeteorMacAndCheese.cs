ed5e686031cbc106a9ebaee5bd8d05cb6675aca0
timoth54 <timoth54@ksu.edu>
2019-09-20 13:39:58 -0500

    Addition of new Side, Meteor Mac and Cheese

---
+++ Menu/Sides/MeteorMacAndCheese.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * MeteorMacAndCheese.cs
+ * Author: Timothy Tucker
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Sides
+{
+    public class MeteorMacAndCheese : Side
+    {
+
+        /// <summary>
+        /// The price of MeteorMacAndCheese.
+        /// </summary>
+        public new double Price
+        {
+            get
+            {
+                switch (this.Size)
+                {
+                    case Size.Small:
+                        return .99;
+                    case Size.Medium:
+                        return 1.45;
+                    default:
+                        return 1.95;
+                }
+            }
+        }
+
+        /// <summary>
+        /// The Calories in MeteorMacAndCheese. 
+        /// </summary>
+        public new uint Calories
+        {
+            get
+            {
+                switch (this.Size)
+                {
+                    case Size.Small:
+                        return 420;
+                    case Size.Medium:
+                        return 490;
+                    default:
+                        return 520;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gives a list of ingredients in MeteorMacAndCheese.
+        /// </summary>
+        public new List<string> Ingredients
+        {
+            get
+            {
+                return new List<string>() { "Macaroni Noodles", "Cheese Product", "Pork Sausage" };
+            }
+        }
+
+        /// <summary>
+        /// Size of the Side.
+        /// </summary>
+        public new Size Size { get; set; }
+
+        /// <summary>
+        /// Makes new MeteorMacAndCheese.
+        /// </summary>
+        public MeteorMacAndCheese()
+        {
+            this.Size = Size.Small;
+        }
+    }
+}
