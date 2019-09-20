be4b855399a78d700f3d967254ada6f7e5d6a8e1
timoth54 <timoth54@ksu.edu>
2019-09-20 13:32:47 -0500

    Addition of new Side, Fryceritops

---
+++ Menu/Sides/Fryceritops.cs
@@ -0,0 +1,79 @@
+﻿/*
+ * Fryceritops.cs
+ * Author: Timothy Tucker
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Sides
+{
+    /// <summary>
+    /// Specification for making Fryceritops.
+    /// </summary>
+    public class Fryceritops : Side
+    {
+
+        /// <summary>
+        /// The price of Fryceritops.
+        /// </summary>
+        public new double Price
+        {
+            get
+            {
+                switch (this.Size)
+                {
+                    case Size.Small:
+                        return .99;
+                    case Size.Medium:
+                        return 1.45;
+                    default:
+                        return 1.95;
+                }
+            }
+        }
+
+        /// <summary>
+        /// The Calories in Fryceritops. 
+        /// </summary>
+        public new uint Calories
+        {
+            get
+            {
+                switch (this.Size)
+                {
+                    case Size.Small:
+                        return 222;
+                    case Size.Medium:
+                        return 365;
+                    default:
+                        return 480;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gives a list of ingredients in Fryceritops.
+        /// </summary>
+        public new List<string> Ingredients
+        {
+            get
+            {
+                return new List<string>() { "Potato", "Salt", "Vegetable Oil" };
+            }
+        }
+
+        /// <summary>
+        /// Size of the Side.
+        /// </summary>
+        public new Size Size { get; set; }
+
+        /// <summary>
+        /// Makes new Fryceritops.
+        /// </summary>
+        public Fryceritops()
+        {
+            this.Size = Size.Small;
+        }
+    }
+}
