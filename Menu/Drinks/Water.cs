3cbc50b81630c94cc488e2179bd174be43461e94
timoth54 <timoth54@ksu.edu>
2019-09-27 21:50:12 -0500

    Added Water and WaterTest classes

---
+++ Menu/Drinks/Water.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Drinks
+{
+    /// <summary>
+    /// Specification for making Water
+    /// </summary>
+   public class Water : Drink
+    {
+
+        private bool lemon = false;
+        
+        /// <summary>
+        /// Gets if there is lemon in the drink. 
+        /// </summary>
+        public bool Lemon
+        {
+            get
+            {
+                return lemon;
+            }
+            private set
+            {
+                lemon = true;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the size of the drink.
+        /// </summary>
+        public override Size Size
+        {
+            get
+            {
+                return this.size;
+            }
+            set
+            {
+                this.size = value;
+            }
+        }
+
+        /// <summary>
+        /// Makes a new Water.
+        /// </summary>
+        public Water()
+        {
+            this.Price = .10;
+            this.Calories = 0;
+        }
+
+        /// <summary>
+        /// Gets the ingredients in the drink.
+        /// </summary>
+        public override List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Water" };
+                if (lemon) ingredients.Add("Lemon");
+                return ingredients;
+            }
+        }
+
+        /// <summary>
+        /// Adds lemon to the drink.
+        /// </summary>
+        public void AddLemon()
+        {
+            lemon = true;
+        }
+    }
+}
---
+++ MenuTest/Drinks/WaterTest.cs
@@ -0,0 +1,132 @@
+﻿/*
+ * WaterTest.cs
+ * Author: Timothy Tucker
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using DinoDiner.Menu.Drinks;
+using DinoDiner.Menu;
+
+namespace MenuTest.Drinks
+{
+    public class WaterTest
+    {
+        //The correct default price, calories, ice, size, and lemon properties.
+        [Fact]
+        public void ShouldHaveCorrectDefaultPrice()
+        {
+            Water water = new Water();
+            Assert.Equal<double>(.10, water.Price);
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectDefaultCalories()
+        {
+            Water water = new Water();
+            Assert.Equal<uint>(0, water.Calories);
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectDefaultIce()
+        {
+            Water water = new Water();
+            Assert.True(water.Ice);
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectDefaultSize()
+        {
+            Water water = new Water();
+            Assert.Equal<Size>(Size.Small, water.Size);
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectDefaultLemon()
+        {
+            Water water = new Water();
+            Assert.False(water.Lemon);
+        }
+
+        //The correct price and calories after changing to small, medium, and large sizes.
+        [Fact]
+        public void ShouldHaveCorrectPriceForSmall()
+        {
+            Water water = new Water();
+            water.Size = Size.Small;
+            Assert.Equal<double>(.10, water.Price);
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectPriceForMedium()
+        {
+            Water water = new Water();
+            water.Size = Size.Medium;
+            Assert.Equal<double>(.10, water.Price);
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectPriceForLarge()
+        {
+            Water water = new Water();
+            water.Size = Size.Large;
+            Assert.Equal<double>(.10, water.Price);
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectCaloriesForSmall()
+        {
+            Water water = new Water();
+            water.Size = Size.Small;
+            Assert.Equal<uint>(0, water.Calories);
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectCaloriesForMedium()
+        {
+            Water water = new Water();
+            water.Size = Size.Medium;
+            Assert.Equal<uint>(0, water.Calories);
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectCaloriesForLarge()
+        {
+            Water water = new Water();
+            water.Size = Size.Large;
+            Assert.Equal<uint>(0, water.Calories);
+        }
+
+        //The correct ice after invoking HoldIce() method.
+        [Fact]
+        public void ShouldHaveCorrectIce()
+        {
+            Water water = new Water();
+            water.HoldIce();
+            Assert.False(water.Ice);
+        }
+
+        //The correct lemon after invoking AddLemon() method.
+        [Fact]
+        public void ShouldHaveCorrectLemon()
+        {
+            Water water = new Water();
+            water.AddLemon();
+            Assert.True(water.Lemon);
+        }
+
+        //The correct ingredients are given.
+        [Fact]
+        public void ShouldHaveCorrectIngredients()
+        {
+            Water water = new Water();
+            List<string> ingredients = water.Ingredients;
+            Assert.Contains<string>("Water", ingredients);
+            water.AddLemon();
+            ingredients = water.Ingredients;
+            Assert.Contains<string>("Lemon", ingredients);
+        }
+    }
+}
