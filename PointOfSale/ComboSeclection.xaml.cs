1de9c81729ccf1b976f12f0d459cfa4fe01cde8d
timoth54 <timoth54@ksu.edu>
2019-10-13 10:15:44 -0500

    Addition of ComboSelection page , and changes to the MainWindow to form-fit to height.

--- PointOfSale/ComboSeclection.xaml.cs
+++ PointOfSale/ComboSeclection.xaml.cs
@@ -1,28 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PointOfSale
 {
     /// <summary>
-    /// Interaction logic for ComboSeclection.xaml
+    /// Interaction logic for ComboSelection.xaml
     /// </summary>
-    public partial class ComboSeclection : Page
+    public partial class ComboSelection : Page
     {
-        public ComboSeclection()
+        public ComboSelection()
         {
             InitializeComponent();
+        }
+
+        private void CustomizeCombo(object sender, System.Windows.RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new CustomizeCombo());
         }
     }
 }
